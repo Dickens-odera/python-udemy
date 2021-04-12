8ca02d258694668a4452c4f4f445bdcfd2763f95
Dickens-odera <dickensodera9@gmail.com>
2021-04-13 02:34:44 +0300

    Added the object class

--- Employee.py
+++ Employee.py
@@ -3,7 +3,7 @@ Employee class that defines the Employee attributes and methods
 """
 
 
-class Employee:
+class Employee(object):
     def __init__(self, first_name, last_name, email, phone, employee_no):
         self.first_name = first_name
         self.last_name = last_name
