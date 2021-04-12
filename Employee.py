92a95687c459d7aca02007ec7b6c2044e7baacb8
Dickens-odera <dickensodera9@gmail.com>
2021-04-12 19:21:42 +0300

    Added setters and getter properties

--- Employee.py
+++ Employee.py
@@ -12,17 +12,26 @@ class Employee:
         self.employee_no = employee_no
         self.employees = []  # An empty list of employees
 
-        if len(self.phone) < 10:
-            raise ValueError('The phone number must not be less than 10 characters long')
+    @property
+    def phone(self):
+        return self._phone
+
+    @phone.setter
+    def phone(self, phone):
+        if len(phone) >= 10:
+            raise ValueError('The phone number must not exceed 10 digits')
+        self._phone = phone
 
     def get_employee_no(self):
         return self.employee_no
 
-    def add_employee(self):
-        new_employee = Employee('John', 'Doe', 'john@test.com', '074534321876', 'EMP7123')
-        self.employees.append(new_employee)
+    def add_employee(self, employee_data):
+        self.employees.append(employee_data)
+        
+    def list_employees(self):
+        print(self.employees)
 
-    def __repr__(self):
+    def __str__(self):
         return f'The employee is {self.first_name} {self.last_name} and is identified by {self.employee_no}'
 
 
@@ -33,5 +42,25 @@ The salaried employee that inherits from the employee class
 
 class SalariedEmployee(Employee):
     def __init__(self, first_name, last_name, email, phone, employee_no, salary_amount):
-        super().__init__(first_name, last_name, email, phone, employee_no)
         self.salary_amount = salary_amount
+        super().__init__(first_name, last_name, email, phone, employee_no)
+
+    @property
+    def salary_amount(self):
+        return self._salary_amount
+
+    @salary_amount.setter
+    def salary_amount(self, amount):
+        if amount < 0:
+            raise ValueError('The salary amount cannot be less than 0')
+        self._salary_amount = amount
+
+    def calculate_net_pay(self, deductions):
+        if deductions > self.salary_amount:
+            return False
+        net_pay = self.salary_amount - deductions
+        print(f'Employee with the identity {self.employee_no} earns a net income of {net_pay:.2f}')
+
+    def __str__(self):
+        return f'Employee {self.first_name} {self.last_name} of identity {self.employee_no} earns a gross pay of {self.salary_amount} '
+
--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from strings import *
 from login import login
 from Employee import *
 
+
 # Working with Numbers
 
 
 def calculate_result():
-    print(f'2 to the power of 3 is {2**3}')
+    print(f'2 to the power of 3 is {2 ** 3}')
     print(f'21 modulo 2 is {21 % 2}')
     print(f'First Value {2 + 2 * 5 - 1}')
     print(f'Second value with parenthesis {(2 + 2) * (5 - 1)}')
@@ -18,4 +19,13 @@ if __name__ == '__main__':
     greetings('Dickens')
     print(index_string('Hello There Index'))
     print(slice_string('abcdefghijkl'))
-    print(convert_to_upper('Hello There {first_name} {second_name} {last_name}'.format(first_name='Dickens', second_name='Ochieng', last_name='Ooko')))
+    print(convert_to_upper(
+        'Hello There {first_name} {second_name} {last_name}'.format(first_name='Dickens', second_name='Ochieng',
+                                                                    last_name='Ooko')))
+
+    employee = Employee('John', 'Doe', 'john@test.com', '07179489', 'EMP1234')
+    employee.add_employee(employee)
+    print(employee.get_employee_no())
+
+    salaried_employee = SalariedEmployee('Dickens', 'Odera', 'dickens@test.com', '07184528', 'EMP4528', 4000)
+    salaried_employee.calculate_net_pay(200)
